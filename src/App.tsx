3790edfcfcb16bcadadbd8d4eb5b3f6b871595a7
kawanishi-LC <hiyoko.n0930@gmail.com>
2024-09-18 23:48:31 +0900

    station2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
