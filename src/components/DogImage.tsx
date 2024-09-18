af44ed780c661d9d5b4b9ace69c7d3ae7cd82cf9
kawanishi-LC <hiyoko.n0930@gmail.com>
2024-09-18 22:55:45 +0900

    Updates

--- src/components/DogImage.tsx
+++
@@ -1,9 +0,0 @@
-export const DogImage = (props: { imageUrl: string | undefined; }) => {
-  return (
-    <>
-      <img src={props.imageUrl} alt="犬の画像" />
-    </>
-  );
-};
-
-export default DogImage;
